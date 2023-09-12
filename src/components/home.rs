5f550a29fde2b0b149160d02ab8e4f4948641760
David Wallace Croft <david@CroftSoft.com>
2023-09-12 07:32:53 -0500

    counter

--- src/components/home.rs
+++ src/components/home.rs
@@ -4,42 +4,18 @@ use dioxus_router::prelude::Link;
 
 #[allow(non_snake_case)]
 pub fn Home(cx: Scope) -> Element {
+  let mut count = use_state(cx, || 0);
   render! {
     div {
       class: "app-home box",
     h1 {
-      "Dallas Rust User Meetup"
-    }
-    a {
-      href: "https://www.meetup.com/dallasrust/",
-      target: "_blank",
-      "https://www.meetup.com/dallasrust/"
-    }
-    br { }
-    a {
-      href: "https://www.meetup.com/dallasrust/",
-      target: "_blank",
-    img {
-      src: "rustacean-flat-happy.svg",
-    }
-    }
+      "CroftSoft Dioxus Static Hydration Prototype"
     }
     div {
-      class: "box",
-      style: "margin-top: 2rem",
-    span {
-      style: "font-weight: bold",
-    "2023 Aug 17 Thu"
-    },
-    p {
-      style: "margin-top: 1rem",
-    "The Dallas Rust User Meetup (DRUM) is seeking a location within",
-    " the Dallas area for our next face-to-face meeting."
-    " We are looking for a site with a laptop projector."
-    " If you would like to host the DRUM, please contact "
-    Link { to: Route::About {}, "us" },
-    "."
+      h1 { "High-Five counter: {count}" }
+      button { onclick: move |_| count += 1, "Up high!" }
+      button { onclick: move |_| count -= 1, "Down low!" }
     }
-    }
+  }
   }
 }
