739229be78244451f870901741f2785354a11cb1
David Wallace Croft <david@CroftSoft.com>
2023-09-14 21:38:33 -0500

    removed prerender main

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
 use dioxus_static_hydration::launch;
-#[cfg(feature = "hydrate")]
-use dioxus_static_hydration::route::Route;
-
-// Generate all routes and output them to the docs path
-#[cfg(feature = "prerender")]
-#[tokio::main]
-async fn main() {
-  pre_cache_static_routes_with_props(
-    &ServeConfigBuilder::new_with_router(
-      dioxus_fullstack::router::FullstackRouterConfig::<Route>::default(),
-    )
-    .assets_path("dist")
-    .incremental(IncrementalRendererConfig::default().static_dir("dist"))
-    .build(),
-  )
-  .await
-  .unwrap();
-}
 
 #[cfg(feature = "hydrate")]
 fn main() {
+  use dioxus_static_hydration::route::Route;
   dioxus_web::launch_with_props(
     dioxus_fullstack::router::RouteWithCfg::<Route>,
     dioxus_fullstack::prelude::get_root_props_from_document()
