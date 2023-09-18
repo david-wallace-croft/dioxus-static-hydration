49819546cd8d060cd4696228b831bbba2a1b0017
David Wallace Croft <david@CroftSoft.com>
2023-09-17 19:54:41 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use dioxus_static_hydration::launch;
 
 #[cfg(feature = "hydrate")]
 fn main() {
-  use dioxus_static_hydration::route::Route;
   dioxus_web::launch_with_props(
-    dioxus_fullstack::router::RouteWithCfg::<Route>,
+    dioxus_fullstack::router::RouteWithCfg::<
+      dioxus_static_hydration::route::Route,
+    >,
     dioxus_fullstack::prelude::get_root_props_from_document()
       .expect("Failed to get root props from document"),
     dioxus_web::Config::default().hydrate(true),
